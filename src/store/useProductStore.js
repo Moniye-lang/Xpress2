939f4a53b2cb1bf6d93ddee884e8d6c6937819e0
Moniye-lang <davidadeniyi269@gmail.com>
2026-02-10 21:02:54 +0100

    push images

--- src/store/useProductStore.js
+++ src/store/useProductStore.js
@@ -2,21 +2,22 @@
 import { create } from 'zustand';
 import { data } from "../pages/Productdata";
 
-// This format ensures the worker is found on live servers
-const worker = new Worker(new URL('../../public/productWorker.js', import.meta.url), {
-  type: 'module'
-});
-export const useProductStore = create((set) => ({
-  filteredProducts: data, // Initialize with data so it's not empty on first load
-  isProcessing: false,
+// By using a string, Vite won't try to "bundle" it from the src folder
+// In production, this file will exist at the domain root.
+const worker = new Worker('/productWorker.js');
 
-  filterProducts: (search, category) => {
-    set({ isProcessing: true });
-    
-    worker.postMessage({ products: data, search, category });
-    
-    worker.onmessage = (e) => {
-      set({ filteredProducts: e.data, isProcessing: false });
-    };
-  }
-}));
+export const useProductStore = create((set) => {
+  // Setup listener once
+  worker.onmessage = (e) => {
+    set({ filteredProducts: e.data, isProcessing: false });
+  };
+
+  return {
+    filteredProducts: data,
+    isProcessing: false,
+    filterProducts: (search, category) => {
+      set({ isProcessing: true });
+      worker.postMessage({ products: data, search, category });
+    },
+  };
+});
