4e4d60d70767733cca4681aff383cdcc7c16b52c
Moniye-lang <davidadeniyi269@gmail.com>
2026-02-10 21:00:35 +0100

    push images

--- src/store/useProductStore.js
+++ src/store/useProductStore.js
@@ -3,8 +3,9 @@ import { create } from 'zustand';
 import { data } from "../pages/Productdata";
 
 // This format ensures the worker is found on live servers
-const worker = new Worker(new URL('../productWorker.js', import.meta.url));
-
+const worker = new Worker(new URL('../../public/productWorker.js', import.meta.url), {
+  type: 'module'
+});
 export const useProductStore = create((set) => ({
   filteredProducts: data, // Initialize with data so it's not empty on first load
   isProcessing: false,
